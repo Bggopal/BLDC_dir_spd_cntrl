c58822feefc3d9b8aaa0aff6cd88d9fc6ac76ea0
Bala <bala@zmail.com>
2021-12-14 23:11:50 +0530

    code push

--- EG2_BLDC/EG2_BLDC.ino
+++ EG2_BLDC/EG2_BLDC.ino
@@ -1,9 +1,11 @@
 #include <Servo.h>
 Servo BLDC;
+byte bldc_pin = 9;
+byte pot_pin = A0;
 
 void setup() 
 {
-  BLDC.attach(9);
+  BLDC.attach(bldc_pin);
   BLDC.writeMicroseconds(1500); // make the ESC high
   delay(5000); // delay to power up
 }
@@ -11,6 +13,6 @@ void setup()
 void loop() 
 {
   int val = analogRead(0); // read input from potentiometer.
-  int pwm = map(val, 0, 1023, 0, 180); // maps potentiometer values to PWM value.
-  BLDC.write(pwm); // Send signal to ESC.
+  int pwm = map(val, 0, 1023, 1100, 1900); // maps potentiometer values to PWM value.
+  BLDC.writeMicroseconds(pwm); // Send signal to ESC.
 }
